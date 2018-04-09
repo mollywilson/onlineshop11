e6da70071d8cc2ac5a5a417b2e922cd552b42870
molly <mollywilson_@outlook.com>
2018-04-09 18:58:37 +0100

    completed tutorial 17

--- frontpage/admin.py
+++ frontpage/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Album
+from .models import Album, Single
 
 admin.site.register(Album)
+admin.site.register(Single)
--- frontpage/models.py
+++ frontpage/models.py
@@ -10,12 +10,14 @@ class Album(models.Model):
     format = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.album_name
+        return self.album_name + " - " + self.album_artist
 
 class Single(models.Model):
     single_name = models.CharField(max_length=250)
     single_artist = models.CharField(max_length=250)
-    single_album = models.ForeignKey(Album, on_delete=models.CASCADE)
     description = models.CharField(max_length=1000)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     picture = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.single_name + " - " + self.single_artist

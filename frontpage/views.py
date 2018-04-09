9544729b32b7eb11f54727bf0d8da86cc1b2b842
molly <mollywilson_@outlook.com>
2018-04-09 18:43:21 +0100

    completed tutorial 16

--- frontpage/views.py
+++ frontpage/views.py
@@ -1,13 +1,17 @@
-from django.http import HttpResponse
+from django.http import Http404
+
 from django.shortcuts import render
 from .models import Album
 
 
 def index(request):
     all_albums = Album.objects.all()
-    context = {'all_albums': all_albums}
-    return render(request, 'frontpage/index.html', context)
+    return render(request, 'frontpage/index.html', {'all_albums': all_albums})
 
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for album: " + str(album_id) + "</h2>")
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Sorry! This album is not for sale right now. Please check back later")
+    return render(request, 'frontpage/detail.html', {'album': album})

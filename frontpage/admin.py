33903e08b7d7cf105e3175af4ce5e060433792fb
molly <mollywilson_@outlook.com>
2018-04-09 19:51:09 +0100

    completed tutorial 17

--- frontpage/admin.py
+++ frontpage/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Album, Single
+from .models import Album, Single, Song
 
 admin.site.register(Album)
 admin.site.register(Single)
+admin.site.register(Song)
--- frontpage/models.py
+++ frontpage/models.py
@@ -12,6 +12,14 @@ class Album(models.Model):
     def __str__(self):
         return self.album_name + " - " + self.album_artist
 
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    song_name = models.CharField(max_length=250)
+    song_time = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.song_name
+
 class Single(models.Model):
     single_name = models.CharField(max_length=250)
     single_artist = models.CharField(max_length=250)

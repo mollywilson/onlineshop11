f9b301389f8acf8fc78b9caccc3c9129e7bd58d4
molly <mollywilson_@outlook.com>
2018-04-11 20:02:54 +0100

    completed tutorial 29

--- frontpage/urls.py
+++ frontpage/urls.py
@@ -4,9 +4,6 @@ from . import views
 app_name = 'frontpage'
 
 urlpatterns = [
-    # /frontpage/
-    url(r'^$', views.index, name='index'),
-
-    # /frontpage/ album_id
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name="detail"),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 ]
--- frontpage/views.py
+++ frontpage/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.views import generic
 from .models import Album
 
+class IndexView(generic.ListView):
+    template_name = 'frontpage/index.html'
+    context_object_name = 'all_albums'
 
-def index(request):
-    all_albums = Album.objects.all()
-    return render(request, 'frontpage/index.html', {'all_albums': all_albums})
+    def get_queryset(self):
+        return Album.objects.all()
 
-
-def detail(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    return render(request, 'frontpage/detail.html', {'album': album})
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'frontpage/detail.html'
